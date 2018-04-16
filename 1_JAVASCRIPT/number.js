4a6ee397c4163d74c61afe251628b1cad8c670f4
Vinicius Tarcisio <tfvinicius@gmail.com>
2018-04-16 17:43:38 -0300

    Subindo primeiro semana - JavaScript

---
+++ 1_JAVASCRIPT/number.js
@@ -0,0 +1,21 @@
+//Tipos de Dados
+//number 
+//IEEE-754 - Standard for Floating-Point
+// binary64 ou Double Precision
+
+var nota = 10;
+
+nota.toExponential(2);
+nota.toFixed(2);
+nota.toPrecision();
+nota.toString();
+nota.valueOf();
+
+0.1 + 0.2;
+//0.30000000000000004
+
+3/0;
+// Infinity
+"AgileCode" * 10;
+//NaN
+
