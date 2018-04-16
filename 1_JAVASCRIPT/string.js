1254260c0abc329f312e70121ab8ceb96cfe55e2
Vinicius Tarcisio <tfvinicius@gmail.com>
2018-04-16 18:45:01 -0300

    Adicionando String

---
+++ 1_JAVASCRIPT/string.js
@@ -0,0 +1,10 @@
+//São imutáveis
+//operação de reatribuição
+//podem ser declaradas com aspas simples ou duplas.
+
+
+var nome = "AgileCode";
+
+nome.charAt(2);
+nome.charCodeAt(0);
+nome.concat("!");
