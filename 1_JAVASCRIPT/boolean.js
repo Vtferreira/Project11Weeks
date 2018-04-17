718a99b66eae166da45519c40adf027f4d958935
Vinicius Tarcisio <tfvinicius@gmail.com>
2018-04-17 19:46:48 -0300

    Adicionando tipo de dado boollean

---
+++ 1_JAVASCRIPT/boolean.js
@@ -0,0 +1,32 @@
+var isento = true;
+var estudante = false;
+
+isento.toString();
+
+
+
+//Cuidado truthy e falsy
+
+!!0 //false
+!!NaN //false
+!!" 
+//false
+
+false
+//false
+!!null
+//false
+
+!!undefined
+//false
+
+
+
+
+var pessoa = {};
+
+!!pessoas //true
+
+var carros = [];
+
+!!carros //true
